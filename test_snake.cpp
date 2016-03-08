7aec56c5c6b80254b992cca0c02b6c2047f84b85
olipratt <olipratt@users.noreply.github.com>
2016-03-08 20:53:20 +0000

    Test changing the snake's direction

--- test_snake.cpp
+++ test_snake.cpp
@@ -23,3 +23,18 @@ TEST_CASE( "Move a snake", "[snake]" ) {
 
     REQUIRE( snake.position() == Point2D<int>(2, 0) );
 }
+
+TEST_CASE( "Change a snake's direction", "[snake]" ) {
+
+    Snake snake {{0, 0}, Direction::right};
+
+    snake.face(Direction::down);
+    snake.move();
+
+    REQUIRE( snake.position() == Point2D<int>(0, 1) );
+
+    snake.face(Direction::right);
+    snake.move();
+
+    REQUIRE( snake.position() == Point2D<int>(1, 1) );
+}
